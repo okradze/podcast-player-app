5235e17a9f122bdfd2d40328ba54f4460686e92a
Mirian <okradzemirian@gmail.com>
2021-07-21 21:58:44 +0400

    colors

---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kTextColor = Color(0xFF464555);
+const kOffsetColor = Colors.white;
+const kPrimaryColor = Color(0xFF5071ed);
+const kSecondaryColor = Color(0xFF806dde);
+const kLightText = Color(0xFF9f9fad);
+const kBackgroundColor = Color(0xFFf7f7f7);
+const kLightBorder = Color(0xFFeee8e8);
