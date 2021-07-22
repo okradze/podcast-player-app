d8e9bceb5ff9e360467c12e56cb06a0da58d1bf7
Mirian <okradzemirian@gmail.com>
2021-07-22 12:21:26 +0400

    listen notes api key in .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:podcast_player_app/colors.dart';
 import 'package:podcast_player_app/screens/tabs_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: '.env');
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    print(dotenv.env['LISTEN_NOTES_API_KEY']);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Podcast Player',
