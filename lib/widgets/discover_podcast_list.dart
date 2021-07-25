cac3f4d8f7d541a2b3fe2095416524de5e671df4
Mirian <okradzemirian@gmail.com>
2021-07-25 20:05:36 +0400

    discover podcast list

---
+++ lib/widgets/discover_podcast_list.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:podcast_player_app/models/discover_podcasts_model.dart';
+import 'package:podcast_player_app/widgets/discover_podcast_item.dart';
+
+class DiscoverPodcastsList extends StatelessWidget {
+  const DiscoverPodcastsList({
+    Key? key,
+    required this.list,
+  }) : super(key: key);
+
+  final DiscoverPodcastsModel list;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 50),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            width: double.infinity,
+            child: Text(
+              list.title,
+              style: Theme.of(context).textTheme.headline5,
+              textAlign: TextAlign.start,
+            ),
+          ),
+          SizedBox(height: 20),
+          Container(
+            height: 130,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            alignment: Alignment.centerLeft,
+            child: ListView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: list.podcasts.length,
+              itemBuilder: (ctx, index) {
+                final podcast = list.podcasts[index];
+                return DiscoverPodcastItem(
+                  key: Key(podcast.id),
+                  podcast: podcast,
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/discover_podcasts_lists.dart
+++ lib/widgets/discover_podcasts_lists.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:podcast_player_app/bloc/discover_podcasts_bloc.dart';
-import 'package:podcast_player_app/widgets/discover_podcast_item.dart';
+import 'package:podcast_player_app/widgets/discover_podcast_list.dart';
 
 class DiscoverPodcastsLists extends StatelessWidget {
   @override
@@ -14,43 +14,7 @@ class DiscoverPodcastsLists extends StatelessWidget {
           itemBuilder: (ctx, index) {
             final list = state.lists[index];
 
-            return Container(
-              margin: const EdgeInsets.only(bottom: 50),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    width: double.infinity,
-                    child: Text(
-                      list.title,
-                      style: Theme.of(context).textTheme.headline5,
-                      textAlign: TextAlign.start,
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Container(
-                    height: 130,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    alignment: Alignment.centerLeft,
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      itemCount: list.podcasts.length,
-                      itemBuilder: (ctx, index) {
-                        final podcast = list.podcasts[index];
-                        return DiscoverPodcastItem(
-                          key: Key(podcast.id),
-                          podcast: podcast,
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            );
+            return DiscoverPodcastsList(list: list);
           },
           itemCount: state.lists.length,
         );
