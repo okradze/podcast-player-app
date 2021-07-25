c3ef734c64f5bd6464a35c11f679c99e3dc5b9e9
Mirian <okradzemirian@gmail.com>
2021-07-25 20:19:56 +0400

    spinner in lists

--- lib/widgets/discover_podcasts_lists.dart
+++ lib/widgets/discover_podcasts_lists.dart
@@ -2,22 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:podcast_player_app/bloc/discover_podcasts_bloc.dart';
 import 'package:podcast_player_app/widgets/discover_podcast_list.dart';
+import 'package:podcast_player_app/widgets/spinner.dart';
 
 class DiscoverPodcastsLists extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<DiscoverPodcastsBloc, DiscoverPodcastsState>(
       builder: (context, state) {
-        return ListView.builder(
-          physics: NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          itemBuilder: (ctx, index) {
-            final list = state.lists[index];
-
-            return DiscoverPodcastsList(list: list);
-          },
-          itemCount: state.lists.length,
-        );
+        switch (state.status) {
+          case DiscoverPodcastsStatus.failure:
+            return Center(child: Text('Something Went Wrong'));
+          case DiscoverPodcastsStatus.success:
+            return Column(
+              children: [
+                ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemBuilder: (ctx, index) {
+                    final list = state.lists[index];
+                    return DiscoverPodcastsList(key: Key(list.id), list: list);
+                  },
+                  itemCount: state.lists.length,
+                ),
+                if (state.hasNextPage) Spinner(),
+              ],
+            );
+          default:
+            return Spinner();
+        }
       },
     );
   }
